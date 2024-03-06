5f8c5e49a9ccd1da24067db986962296bfa113c4
Said Rossi <srossi@ksarcapital.com>
2024-03-06 18:20:26 -0500

    Add project files.

---
+++ Components/Pages/Index.razor.cs
@@ -0,0 +1,43 @@
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using Radzen;
+using Radzen.Blazor;
+
+namespace Ksarcapitalsite.Components.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+        
+        private bool buttonClicked = true;
+        
+        void OnClick(string text)
+        {
+            buttonClicked = !buttonClicked;
+            // NotificationService.Notify(new NotificationMessage { Severity = NotificationSeverity.Info, Summary = "Button Clicked", Detail = text });
+        }
+        }
+}
